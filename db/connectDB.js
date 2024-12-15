972d3421a4b8be7395c2aa39762eaaf42ace9d8f
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-12-15 15:51:22 +0530

    gitignore updated

--- db/connectDB.js
+++ db/connectDB.js
@@ -4,6 +4,7 @@ export const connectDB = async () => {
 	try {
 		console.log("mongo_uri: ", process.env.MONGO_URI);
 		const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.log("Error connection to MongoDB: ", error.message);
