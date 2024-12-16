315f749edb68b85d776401a08de83174d4819477
Arnab Majumder <arnabmajumdar399@gmail.com>
2024-12-16 14:59:57 +0530

    cors fixed

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
     'http://localhost:3000', 
-    'https://admin-store-frontend.vercel.app/',
+    'https://admin-store-frontend.vercel.app',
     // Add your deployed frontend URL here
 ];
 
